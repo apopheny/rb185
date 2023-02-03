1cf578303b19c6214b3b7c4f0f31a4cf23f777eb
Chris Douglass <cedouglass@gmail.com>
2023-02-03 12:52:24 -0500

    update to list func

--- v0.01.rb
+++ v0.01.rb
@@ -1,28 +1,26 @@
 #! /usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'pg'
 require 'date'
 
-@expense_db = PG.connect(dbname:'project1')
+@expense_db = PG.connect(dbname: 'project1')
 
 # @expenses = @expense_db.exec('SELECT * FROM expenses ORDER BY id')
 
 @command = ARGV[0]
 
-
 def display_list
-  @expenses = @expense_db.exec("SELECT * FROM expenses ORDER BY id")
+  @expenses = @expense_db.exec('SELECT * FROM expenses ORDER BY id')
   @expenses.each do |tuple|
-    columns = [ tuple["id"].rjust(3),
-                tuple["created_on"].rjust(10),
-                tuple["amount"].rjust(12),
-                tuple["memo"] ]
-  
-    puts columns.join(" | ")
+    columns = [tuple['id'].rjust(3),
+               tuple['created_on'].rjust(10),
+               tuple['amount'].rjust(12),
+               tuple['memo']]
+
+    puts columns.join(' | ')
   end
 end
-
-
 
 def display_help
   puts "An expense recording system\n\n"
@@ -31,31 +29,30 @@ end
 
 def validate_add(cost:, item:, date:)
   return 'Please enter a valid amount' unless cost.to_f > 0.0
-  
-  case 
-    when !cost
-      "You must specify an AMOUNT\n\n"
-    when !item
-      "You must specify a MEMO\n\n"
-    when date
-      begin
-        Date.valid_date?(*date.to_s.split('-').map(&:to_i))
-      rescue ArgumentError, NameError
-        return "The date you have entered is invalid." 
-      else
-        false
-      end
+
+  if !cost
+    "You must specify an AMOUNT\n\n"
+  elsif !item
+    "You must specify a MEMO\n\n"
+  elsif date
+    begin
+      Date.valid_date?(*date.to_s.split('-').map(&:to_i))
+    rescue ArgumentError, NameError
+      'The date you have entered is invalid.'
     else
       false
+    end
+  else
+    false
   end
 end
 
-def add_expense(cost: ARGV[1], item:ARGV[2], date:ARGV[3])
+def add_expense(cost: ARGV[1], item: ARGV[2], date: ARGV[3])
   error_help = validate_add(cost: cost, item: item, date: date)
-  
+
   if !error_help
-    @expense_db.exec_params("INSERT INTO expenses(amount, memo, created_on)"\
-      "VALUES ($1, $2, $3);", [cost, item, (date ? date : Date.today.to_s)]).values
+    @expense_db.exec_params('INSERT INTO expenses(amount, memo, created_on)'\
+      'VALUES ($1, $2, $3);', [cost, item, (date || Date.today.to_s)]).values
   else
     puts error_help
     puts @help
